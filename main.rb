6e9b0fbe596da40820df0126cf1a40c335aef497
glooer <kostya14@gmail.com>
2016-07-18 04:14:16 +0500

    количество потоков вынесено в отдельную константу

--- main.rb
+++ main.rb
@@ -4,8 +4,10 @@ require "open-uri" #for open url
 # auth
 # https://oauth.vk.com/authorize?client_id=4787098&display=page&redirect_uri=https://oauth.vk.com/blank.html &scope=audio,nohttps&response_type=token&v=5.52
 
-ACCESS_TOKEN = ""
-SECRET = ""
+THREAD_COUNT = 4; #количество потоков
+
+ACCESS_TOKEN = "a4508400baba2a626049fc8cba0197e9a517b05f7bde5ec8bd5d933e991ed8456385b7e1209ca81412e12"
+SECRET = "7aae67b0db223f0c0f"
 
 OWNER_ID = 114095390
 
@@ -31,7 +33,7 @@ if (musics = music["response"])
   end
   all_count = work.length
   in_thread = 0
-  workers = (0..4).map do 
+  workers = (0...THREAD_COUNT).map do 
     Thread.new do
       begin
         while x = work.pop(true)
