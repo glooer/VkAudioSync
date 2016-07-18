3fe5baae730d647b0c9bd1561c3ad85420b5e5ca
glooer <kostya14@gmail.com>
2016-07-18 12:59:09 +0500

    настройки вынесены в отдельный файл

---
+++ config.rb
@@ -0,0 +1,9 @@
+
+THREAD_COUNT = 4; #количество потоков
+# auth
+# https://oauth.vk.com/authorize?client_id=4787098&display=page&redirect_uri=https://oauth.vk.com/blank.html &scope=audio,nohttps&response_type=token&v=5.52
+
+ACCESS_TOKEN = "1eb012004011f9a8d4ce6e70d4075b22d0794296f20a38a4ed16f134c84673ba3933e74cc2c5adc44798a"
+SECRET = "a5f3805f79d0cd15e6"
+
+OWNER_ID = 20882339
--- main.rb
+++ main.rb
@@ -1,15 +1,6 @@
 require "./VK.rb"
+require "./config.rb"
 require "open-uri" #for open url
-
-# auth
-# https://oauth.vk.com/authorize?client_id=4787098&display=page&redirect_uri=https://oauth.vk.com/blank.html &scope=audio,nohttps&response_type=token&v=5.52
-
-THREAD_COUNT = 4; #количество потоков
-
-ACCESS_TOKEN = "1eb012004011f9a8d4ce6e70d4075b22d0794296f20a38a4ed16f134c84673ba3933e74cc2c5adc44798a"
-SECRET = "a5f3805f79d0cd15e6"
-
-OWNER_ID = 20882339
 
 vk = VK.new ACCESS_TOKEN, SECRET
 
